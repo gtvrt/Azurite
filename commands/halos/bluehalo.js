1e20d6d4abab6b45384bea8684f7005d42c4c514
gtvrt <56785230+gtvrt@users.noreply.github.com>
2020-05-14 21:20:44 +0800

    Update bluehalo.js

--- commands/halos/bluehalo.js
+++ commands/halos/bluehalo.js
@@ -8,7 +8,7 @@ module.exports = {
 
         var bluehalo = new RichEmbed()
             .setDescription("Blue Halo")
-            .setColor("#BB0A1E")
+            .setColor("BLUE")
             .addField("Halo:", "Blue")
             .addField("Ways to get:", "Finish 169 stages and get the explorer badge.")
             .setAuthor("Halo Grinders | Red Halo", "https://cdn.discordapp.com/icons/709007697626005566/6d2b1f1882f674e06a41280dbb0e970c.png?size=256")
