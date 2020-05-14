d80dd401f60b1ab88b99181759974ccdb708161b
gtvrt <56785230+gtvrt@users.noreply.github.com>
2020-05-14 21:05:16 +0800

    Update redhalo.js

--- commands/halos/redhalo.js
+++ commands/halos/redhalo.js
@@ -7,7 +7,7 @@ module.exports = {
 
         var redhalo = new RichEmbed()
             .setDescription("Red Halo")
-            .setColor("#003bff")
+            .setColor("#BB0A1E")
             .addField("Halo:", "Red")
             .addField("Ways to get:", "Finish THE Tower of hell which has all stages.")
             .setAuthor("Halo Grinders | Red Halo", "https://cdn.discordapp.com/icons/709007697626005566/6d2b1f1882f674e06a41280dbb0e970c.png?size=256")
