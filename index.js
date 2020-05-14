3d80730f00a8baea90d2ea77b34ca556831d2860
gtvrt <56785230+gtvrt@users.noreply.github.com>
2020-05-14 20:50:35 +0800

    Update index.js

--- index.js
+++ index.js
@@ -57,7 +57,7 @@ client.on("ready", () => {
 });
 
 client.on("message", async message => {
-    const prefix = "!";
+    const prefix = "h!";
 
     if (message.author.bot) return;
     if (!message.guild) return;
