90222beb9f89adb704be4f9c6ee73bf5856b1445
gtvrt <56785230+gtvrt@users.noreply.github.com>
2020-05-14 21:13:34 +0800

    Update bluehalo.js

--- commands/halos/bluehalo.js
+++ commands/halos/bluehalo.js
@@ -1,23 +1,21 @@
+  
 const { discord, RichEmbed } = require("discord.js");
 module.exports = {
-    name: "serverinfo",
-    category: "info",
-    description: "Shows the server's info.",
+    name: "redhalo",
+    category: "halos",
+    description: "How to get a red halo?",
     run: async (client, message, args) => {
 
-        var serverEmbed = new RichEmbed()
-            .setDescription("Server Info")
-            .setColor("#003bff")
-            .addField("Bot name:", client.user.username)
-            .addField("Total Members:", message.guild.memberCount)
-            .setAuthor("Halo Grinders | Info", "https://cdn.discordapp.com/icons/709007697626005566/6d2b1f1882f674e06a41280dbb0e970c.png?size=256")
+        var bluehalo = new RichEmbed()
+            .setDescription("Blue Halo")
+            .setColor("#BB0A1E")
+            .addField("Halo:", "Blue")
+            .addField("Ways to get:", "Finish 169 stages and get the explorer badge.")
+            .setAuthor("Halo Grinders | Red Halo", "https://cdn.discordapp.com/icons/709007697626005566/6d2b1f1882f674e06a41280dbb0e970c.png?size=256")
             .setTimestamp()
-            .setFooter("Halo Grinders | Info", "https://cdn.discordapp.com/icons/709007697626005566/6d2b1f1882f674e06a41280dbb0e970c.png?size=256");
+            .setFooter("Halo Grinders | Red Halo", "https://cdn.discordapp.com/icons/709007697626005566/6d2b1f1882f674e06a41280dbb0e970c.png?size=256");
 
 
-
-
-        message.channel.send(serverEmbed);
+        message.channel.send(bluehalo);
         }
     }
-    
