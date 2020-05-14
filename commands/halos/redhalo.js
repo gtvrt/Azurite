7073cd6275beccc12ac022edd9c04c77ac74dc28
gtvrt <56785230+gtvrt@users.noreply.github.com>
2020-05-14 21:02:43 +0800

    Update redhalo.js

--- commands/halos/redhalo.js
+++ commands/halos/redhalo.js
@@ -1,23 +1,21 @@
 const { discord, RichEmbed } = require("discord.js");
 module.exports = {
-    name: "serverinfo",
-    category: "info",
-    description: "Shows the server's info.",
+    name: "redhalo",
+    category: "halo",
+    description: "How to get a red halo?",
     run: async (client, message, args) => {
 
-        var serverEmbed = new RichEmbed()
-            .setDescription("Server Info")
+        var redhalo = new RichEmbed()
+            .setDescription("Red Halo")
             .setColor("#003bff")
-            .addField("Bot name:", client.user.username)
-            .addField("Total Members:", message.guild.memberCount)
-            .setAuthor("Halo Grinders | Info", "https://cdn.discordapp.com/icons/709007697626005566/6d2b1f1882f674e06a41280dbb0e970c.png?size=256")
+            .addField("Halo:", "Red")
+            .addField("Ways to get:", "Finish THE Tower of hell which has all stages.")
+            .setAuthor("Halo Grinders | Red Halo", "https://cdn.discordapp.com/icons/709007697626005566/6d2b1f1882f674e06a41280dbb0e970c.png?size=256")
             .setTimestamp()
-            .setFooter("Halo Grinders | Info", "https://cdn.discordapp.com/icons/709007697626005566/6d2b1f1882f674e06a41280dbb0e970c.png?size=256");
+            .setFooter("Halo Grinders | Red Halo", "https://cdn.discordapp.com/icons/709007697626005566/6d2b1f1882f674e06a41280dbb0e970c.png?size=256");
 
 
-
-
-        message.channel.send(serverEmbed);
+        message.channel.send(redhalo);
         }
     }
     
