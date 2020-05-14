c9cdc4d0fed1df8bba062bde6b94c3564ca42678
gtvrt <56785230+gtvrt@users.noreply.github.com>
2020-05-14 21:08:05 +0800

    Update serverinfo.js

--- commands/info/serverinfo.js
+++ commands/info/serverinfo.js
@@ -8,8 +8,8 @@ module.exports = {
         var serverEmbed = new RichEmbed()
             .setDescription("Server Info")
             .setColor("#000000")
-            .addField("Bot name:", client.user.username)
-            .addField("Total Members:", message.guild.memberCount)
+            .addField("Bot name:", client.user.username, true)
+            .addField("Total Members:", message.guild.memberCount, true)
             .setAuthor("Halo Grinders | Info", "https://cdn.discordapp.com/icons/709007697626005566/6d2b1f1882f674e06a41280dbb0e970c.png?size=256")
             .setTimestamp()
             .setFooter("Halo Grinders | Info", "https://cdn.discordapp.com/icons/709007697626005566/6d2b1f1882f674e06a41280dbb0e970c.png?size=256");
